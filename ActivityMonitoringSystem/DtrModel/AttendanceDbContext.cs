8e8c573b693ede6a991167ff72b9b8528ac6689f
johncabugao <johncabugao@247international.net>
2017-05-30 14:25:20 +0800

    add dbcontext

---
+++ ActivityMonitoringSystem/DtrModel/AttendanceDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.Entity;
+using DtrModel;
+
+namespace SQLiteExample
+{
+    public class AttendanceDbContext : DbContext
+    {
+        public AttendanceDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+            Configure();
+        }
+
+        public AttendanceDbContext(DbConnection connection, bool contextOwnsConnection)
+            : base(connection , contextOwnsConnection)
+        {
+            Configure();
+        }
+
+        private void Configure()
+        {
+            Configuration.ProxyCreationEnabled = true;
+            Configuration.LazyLoadingEnabled = true;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            ModelConfiguration.Configure(modelBuilder);
+            var initializer = new AttendanceDBInitializer(modelBuilder);
+            Database.SetInitializer(initializer);
+        }
+    }
+}
