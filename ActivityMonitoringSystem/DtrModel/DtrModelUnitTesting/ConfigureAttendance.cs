e82cadb9f48ee8070b9b31b535c2f3013e339c64
johncabugao <johncabugao@247international.net>
2017-05-30 15:22:24 +0800

    updated Unit testing

--- ActivityMonitoringSystem/DtrModel/DtrModelUnitTesting/ConfigureAttendance.cs
+++ ActivityMonitoringSystem/DtrModel/DtrModelUnitTesting/ConfigureAttendance.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace DtrModelUnitTesting
 {
     [TestClass]
-    public class UnitTest1
+    public class ConfigureAttendance
     {
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
