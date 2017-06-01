fda1335c2063cf27fddeccf1cd4c87f5a22ebb08
jcpflores <jcp.flores@247international.net>
2017-06-01 11:41:37 +0800

    Implemented IController from DtrInterfaces

--- ActivityMonitoringSystem/DtrController/Controller.cs
+++ ActivityMonitoringSystem/DtrController/Controller.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DtrInterfaces;
 
 namespace DtrController
 {
-    public class Controller
+    public class Controller : IController
     {
     }
 }
