83875e047f302366c76de62df4cebef387d7029b
johncabugao <johncabugao@247international.net>
2017-06-05 14:34:01 +0800

    DBSet Context Added

--- ActivityMonitoringSystem/DtrModel/AttendanceDbContext.cs
+++ ActivityMonitoringSystem/DtrModel/AttendanceDbContext.cs
@@ -5,11 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Common;
 using System.Data.Entity;
+using DtrModel.Entities;
 
 namespace DtrModel
 {
+ 
     public class AttendanceDbContext : DbContext
     {
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<AttendanceSummary> Attendance { get; set; }
+        public DbSet<Client> Client { get; set; }
+        public DbSet<ClientContract> ClientContract { get; set; }
+        public DbSet<ClientHoliday> ClientHoliday { get; set; }
+        public DbSet<DailyTimeRecord> DailyTimeRecord { get; set; }
+        public DbSet<Holiday> Holiday { get; set; }
+        public DbSet<ProcessRole> ProcessRole { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<SkillLevel> SkillLevel { get; set; }
+        public DbSet<TechnicalRole> TechnicalRole { get; set; }
+        public DbSet<TimeoffReason> TimeOffReason { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<WorkLocation> WorkLoc { get; set; }
+        public DbSet<WorkSchedule> WorkSchedule { get; set; }
+ 
         public AttendanceDbContext(string nameOrConnectionString)
             : base(nameOrConnectionString)
         {
