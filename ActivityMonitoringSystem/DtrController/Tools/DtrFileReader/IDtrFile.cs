6411b6f8fa5c08de794b39ad35eb9acae130bc66
DioshicoPh4nt0m <jayamores@247international.net>
2017-06-14 17:20:35 +0800

    update for IDtrFile

--- ActivityMonitoringSystem/DtrController/Tools/DtrFileReader/IDtrFile.cs
+++ ActivityMonitoringSystem/DtrController/Tools/DtrFileReader/IDtrFile.cs
@@ -9,28 +9,31 @@ namespace DtrController.Tools.DtrFileReader
 {
     public interface IDtrFile 
     {
-       
 
+        void SetController(DtrFileReader controller);
         // Get File to read
         string getDtrFilePath { get; set; }
         string Sheet { get; set; }
+        bool Status { get; set; }
 
         
 
-        DateTime Date { get; set; }
-        DateTime TimeIn { get; set; }
-        DateTime TimeOut { get; set; }
-        String ProcessRole { get; set; }
-        String ClientName { get; set; }
-        String Contact { get; set; }
-        String Project { get; set; }
-        String WorkLocation { get; set; }
-        String TimeInSchedule { get; set; }
-        String Notes { get; set; }
-        String Late { get; set; }
-        String Overtime { get; set; }
-        String Undertime { get; set; }
-          
-
+        DateTime DateIn { get; set; }
+        DateTime DateOut { get; set; }
+        string TimeIn { get; set; }
+        string TimeOut { get; set; }
+        string ProcessRole { get; set; }
+        string ClientName { get; set; }
+        string Contact { get; set; }
+        string Project { get; set; }
+        string WorkLocation { get; set; }
+        string TimeInSchedule { get; set; }
+        string TimeOffReason { get; set; }
+        string Notes { get; set; }
+        int Late { get; set; }
+        int Overtime { get; set; }
+        int Undertime { get; set; }
+        Nullable<int> WorkingHours { get; set; }
+        String ResourceId { get; set; }
     }
 }
