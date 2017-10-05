e00e18a4b14d824985e7a7dbf6bbf0294c0b8d02
jcpflores <jcp.flores@247international.net>
2017-10-05 16:32:48 +0800

    Added main application

---
+++ ActivityMonitoringSystem/AttendanceApp/AttendanceApp/Main.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace AttendanceApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUploadDtr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUploadDtr
+            // 
+            this.btnUploadDtr.Location = new System.Drawing.Point(12, 82);
+            this.btnUploadDtr.Name = "btnUploadDtr";
+            this.btnUploadDtr.Size = new System.Drawing.Size(125, 23);
+            this.btnUploadDtr.TabIndex = 0;
+            this.btnUploadDtr.Text = "Upload DTR";
+            this.btnUploadDtr.UseVisualStyleBackColor = true;
+            this.btnUploadDtr.Click += new System.EventHandler(this.btnUploadDtr_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 463);
+            this.Controls.Add(this.btnUploadDtr);
+            this.Name = "Main";
+            this.Text = "Attendance Monitoring";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUploadDtr;
+    }
+}
+
---
+++ ActivityMonitoringSystem/AttendanceApp/AttendanceApp/Main.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DtrDelegates;
+using DtrCommon;
+using DtrController;
+
+namespace AttendanceApp
+{
+    public partial class Main : Form, DtrInterfaces.IView
+    {
+        DtrController.Controller _controller = null;
+        public Main()
+        {
+            InitializeComponent();
+
+            _controller = new Controller();
+            _controller.SetView(this);
+        }
+
+        #region IView
+        public event GetFilesFromLocalEventHandler GetFilesFromLocalEvent;
+
+        public event GetFilesFromRemoteEventHandler GetFilesFromRemoteEvent;
+
+        public event ParseFilesEventHandler ParseFilesEvent;
+
+        public event GetDtrDetailsEventHandler GetDtrDetailsEvent;
+
+        public event SaveDtrInfoEventHandler SaveDtrInfoEvent;
+
+        public void ShowFiles(string[] discoveredFiles)
+        { }
+
+        public void ShowProcessedResources(List<ProcessedResource> processed)
+        { }
+
+        public void ShowDtrInfo(DtrInfo info)
+        { }
+
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+        #endregion
+
+        private void btnUploadDtr_Click(object sender, EventArgs e)
+        {
+            string source = string.Empty;
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                openFile.Multiselect = false;
+                openFile.CheckPathExists = true;
+                openFile.Filter = "Excel | *.xlsx";
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    source = openFile.FileName;
+                }
+            }
+
+            GetFilesFromLocalEvent?.Invoke(source);
+        }
+    }
+}
