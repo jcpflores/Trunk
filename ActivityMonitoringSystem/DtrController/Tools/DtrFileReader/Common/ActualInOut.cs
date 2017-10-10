4ef2e8cd7b46c05173b91943967af4975f6261e8
johncabugao <johncabugao@247international.net>
2017-10-10 16:16:14 +0800

    ActualInOut class added

---
+++ ActivityMonitoringSystem/DtrController/Tools/DtrFileReader/Common/ActualInOut.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtrController.Tools.DtrFileReader.Common
+{
+    public class ActualInOut
+    {
+        
+        public string DateIn { get; set; }
+        public string DateOut { get; set; }
+        public DateTime TimeIn { get; set; }
+        public DateTime TimeOut { get; set; }
+        public string WorkHours { get; set; }
+        public string WorkLocation { get; set; }
+        public string TimeOffReason { get; set; }
+        public int BillableWorkHours { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
