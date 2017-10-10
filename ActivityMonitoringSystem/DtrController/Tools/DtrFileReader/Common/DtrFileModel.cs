538f1be98687db206aed3f51d970ea199d2e3ecb
johncabugao <johncabugao@247international.net>
2017-10-10 17:03:11 +0800

    removed other fields in DtrFileModel

--- ActivityMonitoringSystem/DtrController/Tools/DtrFileReader/Common/DtrFileModel.cs
+++ ActivityMonitoringSystem/DtrController/Tools/DtrFileReader/Common/DtrFileModel.cs
@@ -13,26 +13,18 @@ namespace DtrController.Tools.DtrFileReader.Common
         public DtrFileModel()
         { }
 
-        public string ResourceId { get; set; }      
-        public string TimeIn { get; set; }
-        public string TimeOut { get; set; }
+        public string ResourceId { get; set; }
         public string ProcessRole { get; set; }
         public string ClientName { get; set; }
         public string ContractRef { get; set; }
         public string Project { get; set; }
         public string WorkLocation { get; set; }
         public DateTime TimeInSchedule { get; set; }
-        public string Notes { get; set; }
-        public string TimeOffReason { get; set; }
-        public int Late { get; set; }
-        public int Overtime { get; set; }
-        public string Undertime { get; set; }
-        public Nullable<int> WorkingHours { get; set; }   
         public string TechnicalRole { get; set; }
         public string Technology { get; set; }
-        public string SkillLevel { get; set; }            
+        public string SkillLevel { get; set; }
         public DateTime MonthYear { get; set; }
-      
+
 
         public ICollection<ActualInOut> ActualInOut { get; set; }
     }
